0c3be6510cdddb56a04aa7eeefdce88326559421
Bryan <lazyshot@gmail.com>
2012-12-07 10:15:17 -0500

    WIP - adding support for launching storm topologies with only php

--- lib/storm.php
+++ lib/storm.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once 'storm/storm_types.php';
+require_once 'storm/Nimbus.php';
+
 interface iShellBolt {}
 interface iShellSpout {}
 
@@ -17,53 +20,53 @@ class Tuple
 	}
 }
 
-abstract class ShellComponent
+abstract class BaseShellComponent
 {
 	protected $pid;
 	protected $stormConf;
 	protected $topologyContext;
-	
+
 	protected $stormInc = null;
 	protected $_DEBUG = false;
-	
+
 	public function __construct($debug = false)
-	{		
+	{
 		$this->pid = getmypid();
 		$this->sendCommand(array( "pid" => $this->pid ));
-		
+
 		$this->_debug = $debug;
-		
+
 		if ($this->_DEBUG)
 		{
 			$this->stormInc = fopen('/tmp/' . $this->pid . "_" . strtolower($_SERVER['argv'][0]) . '.txt', 'w+');
 		}
-		
-		$pidDir = $this->readLine();		
+
+		$pidDir = $this->readLine();
 		@fclose(@fopen($pidDir . "/" . $this->pid, "w"));
-		
+
 		$this->stormConf = $this->parseMessage( $this->waitForMessage() );
 		$this->topologyContext = $this->parseMessage( $this->waitForMessage() );
 	}
-	
+
 	protected function readLine()
 	{
 		$line = trim(fgets(STDIN));
-		
+
 		if ($this->_DEBUG)
 		{
 			fputs($this->stormInc, $line . "\n");
 		}
-		
+
 		return $line;
 	}
-	
+
 	protected function waitForMessage()
 	{
 		$message = '';
 		while (true)
 		{
 			$line = trim($this->readLine());
-			
+
 			if (strlen($line) == 0)
 			{
 				continue;
@@ -77,18 +80,18 @@ abstract class ShellComponent
 				$message = '';
 				continue;
 			}
-			
-			$message .= $line . "\n";			
+
+			$message .= $line . "\n";
 		}
-		
+
 		return trim($message);
 	}
-	
+
 	protected function sendCommand(array $command)
 	{
 		$this->sendMessage(json_encode($command));
 	}
-	
+
 	protected function sendLog($message)
 	{
 		return $this->sendCommand(array(
@@ -96,11 +99,11 @@ abstract class ShellComponent
 			'msg' => $message
 		));
 	}
-	
+
 	protected function parseMessage($message)
 	{
 		$msg = json_decode($message, true);
-		
+
 		if ($msg)
 		{
 			return $msg;
@@ -110,75 +113,75 @@ abstract class ShellComponent
 			return $message;
 		}
 	}
-	
+
 	protected function sendMessage($message)
 	{
 		echo $message . "\n";
 		echo "end\n";
-		fflush(STDOUT);	
+		fflush(STDOUT);
 	}
-	
+
 }
 
-abstract class ShellBolt extends ShellComponent implements iShellBolt {
+abstract class ShellBolt extends BaseShellComponent implements iShellBolt {
 
 	public $anchor_tuple = null;
-	
+
 	public function __construct()
 	{
 		parent::__construct();
-		
+
 		$this->init($this->stormConf, $this->topologyContext);
 	}
-	
+
 	public function run()
 	{
 		try {
 			while(true)
 			{
 				$command = $this->parseMessage( $this->waitForMessage() );
-								
+
 				if (is_array($command))
 				{
 					if (isset($command['tuple']))
 					{
 						$tupleMap = array_merge(array(
-							'id' => null, 
-							'comp' => null, 
-							'stream' => null, 
-							'task' => null, 
+							'id' => null,
+							'comp' => null,
+							'stream' => null,
+							'task' => null,
 							'tuple' => null
 						),
-						
+
 						$command);
-		
+
 						$tuple = new Tuple($tupleMap['id'], $tupleMap['comp'], $tupleMap['stream'], $tupleMap['task'], $tupleMap['tuple']);
-						
+
 						$this->process($tuple);
-						
+
 						$this->sync();
 					}
 				}
 			}
-		} 
+		}
 		catch(Exception $e)
 		{
 			$this->sendLog( $e->getTraceAsSTring() );
 		}
 	}
-	
+
 	abstract protected function process(Tuple $tuple);
-	
+
 	protected function init($conf, $topology)
 	{
 		return;
 	}
-		
+
 	protected function sync()
 	{
 		$this->sendCommand(array( 'command' => 'sync' ));
 	}
-	
+
 	protected function emitTuple(array $tuple, $stream = null, $anchors = array(), $directTask = null)
 	{
 		if ($this->anchor_tuple !== null)
@@ -189,7 +192,7 @@ abstract class ShellBolt extends ShellComponent implements iShellBolt {
 		$command = array(
 			'command' => 'emit'
 		);
-		
+
 		if($stream !== null)
 		{
 			$command['stream'] = $stream;
@@ -225,7 +228,7 @@ abstract class ShellBolt extends ShellComponent implements iShellBolt {
 			'command' => 'ack',
 			'id' => $tuple->id
 		);
-		
+
 		$this->sendCommand($command);
 	}
 
@@ -235,7 +238,7 @@ abstract class ShellBolt extends ShellComponent implements iShellBolt {
 			'command' => 'fail',
 			'id' => $tuple->id
 		);
-		
+
 		$this->sendCommand($command);
 	}
 }
@@ -248,25 +251,25 @@ abstract class BasicBolt extends ShellBolt
 			while(true)
 			{
 				$command = $this->parseMessage( $this->waitForMessage() );
-				
+
 				if (is_array($command))
 				{
 					if (isset($command['tuple']))
 					{
 						$tupleMap = array_merge(array(
-							'id' => null, 
-							'comp' => null, 
-							'stream' => null, 
-							'task' => null, 
+							'id' => null,
+							'comp' => null,
+							'stream' => null,
+							'task' => null,
 							'tuple' => null
 						),
-						
+
 						$command);
-						
+
 						$tuple = new Tuple($tupleMap['id'], $tupleMap['comp'], $tupleMap['stream'], $tupleMap['task'], $tupleMap['tuple']);
-						
+
 						$this->anchor_tuple = $tuple;
-						
+
 						try
 						{
 							$processed = $this->process($tuple);
@@ -277,46 +280,46 @@ abstract class BasicBolt extends ShellBolt
 						{
 							$this->fail($tuple);
 						}
-						
+
 						$this->sync();
 					}
 				}
 			}
-		} 
+		}
 		catch(Exception $e)
 		{
 			$this->sendLog($e->getTraceAsSTring());
 		}
-		
+
 	}
 }
 
-abstract class ShellSpout extends ShellComponent implements iShellSpout
+abstract class ShellSpout extends BaseShellComponent implements iShellSpout
 {
 	protected $tuples = array();
-	
+
 	public function __construct()
 	{
 		parent::__construct();
-		
+
 		$this->init($this->stormConf, $this->topologyContext);
 	}
-	
-	
-	abstract protected function nextTuple();	
+
+
+	abstract protected function nextTuple();
 	abstract protected function ack($tuple_id);
 	abstract protected function fail($tuple_id);
-	
+
 	public function run()
 	{
 		while (true)
 		{
 			$command = $this->parseMessage( $this->waitForMessage() );
-			
+
 			if (is_array($command))
 			{
 				if (isset($command['command']))
-				{					
+				{
 					if ($command['command'] == 'ack')
 					{
 						$this->ack($command['id']);
@@ -333,33 +336,33 @@ abstract class ShellSpout extends ShellComponent implements iShellSpout
 			}
 		}
 	}
-	
+
 	protected function init($stormConf, $topologyContext)
 	{
 		return;
 	}
-	
+
 	final protected function emit(array $tuple, $messageId = null, $streamId = null)
 	{
 		return $this->emitTuple($tuple, $messageId, $streamId, null);
 	}
-	
+
 	final protected function emitDirect($directTask, array $tuple, $messageId = null, $streamId = null)
 	{
 		return $this->emitTuple($tuple, $messageId, $streamId, $directTask);
 	}
-	
+
 	final private function emitTuple(array $tuple, $messageId = null, $streamId = null, $directTask = null)
 	{
 		$command = array(
 			'command' => 'emit'
 		);
-		
+
 		if ($messageId !== null)
 		{
 			$command['id'] = $messageId;
 		}
-		
+
 		if ($streamId !== null)
 		{
 			$command['stream'] = $streamId;
@@ -377,3 +380,56 @@ abstract class ShellSpout extends ShellComponent implements iShellSpout
 }
 
 class BoltProcessException extends Exception {}
+
+class TopologyBuilder {
+	private $client;
+	private $socket;
+	private $transport;
+	private $protocol;
+
+	private $conf = array();
+
+	private $spouts = array();
+	private $bolts = array();
+	private $state_spouts = array();
+
+	public function __construct($host = "localhost", $port = 6627) {
+		$this->socket = new TSocket($host, $port);
+		$this->transport = new TBufferedTransport($this->socket);
+		$this->protocol = new TBinaryProtocol($this->transport);
+		$this->client = new NimbusClient($this->protocol);
+	}
+
+	public function setConf($name, $value) {
+		$conf[$name] = $value;
+	}
+
+	public function addSpout($name, $file, $parallelism_hint = false) {
+		$this->spouts[$name] = array(
+			'spout_object' => new ShellComponent(array('execution_command' => 'php', 'script' => $file)),
+			'common' => array('inputs' => array(), 'streams' => array())
+		);
+
+		if ($parallelism_hint !== false)
+			$this->spouts[$name]['common']['parallelism_hint'] = $parallelism_hint;
+	}
+
+	public function addBolt($name, $file, $parallelism_hint = false) {
+		$this->bolts[$name] = array(
+			'bolt_object' => new ShellComponent(array(
+				'execution_command' => 'php',
+				'script' => $file)
+			),
+			'common' => array('inputs' => array(), 'streams' => array())
+		);
+	}
+
+	public function addSpoutStream($from, $to, array $fields, $direct = false) {
+		$this->spouts[$name]['common']['streams'][$to] = new StreamInfo(array('fields' => $fields, 'direct' => $direct));
+	}
+
+	public function addBoltStream($from, $to, array $fields, $direct = false) {
+		$this->bolts[$name]['common']['streams'][$to] = new StreamInfo(array('fields' => $fields, 'direct' => $direct));
+	}
+
+}
